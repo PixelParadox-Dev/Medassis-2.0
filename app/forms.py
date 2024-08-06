7d314177c5b6833f33b86f8ca8c50ea7ca9b381e
Rish <rishabhkartik2004@gmail.com>
2024-08-06 20:12:18 +0530

    Added forms

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField,RadioField,IntegerField,SelectField,DecimalField
+from wtforms import StringField, PasswordField, SubmitField,RadioField,IntegerField,SelectField,DecimalField,SelectMultipleField
 from wtforms.validators import Length, Email, DataRequired, ValidationError
 
 class DiabetesForm(FlaskForm):
@@ -16,6 +16,71 @@ class DiabetesForm(FlaskForm):
     height=DecimalField("Height (in m)",validators=[DataRequired()],places=2)
     hba1clvl=SelectField("HbA1c Level",validate_choice=[DataRequired()],choices=[('3below','below 3'),('34','3-4'),('45','4-5'),('56','5-6'),('67','6-7'),('78','7-8'),('89','8-9'),('9above','above 9')])
     blood_glucose=IntegerField('Blood Glucose Level',validators=[DataRequired()])
-    email=StringField('Email',validators=[DataRequired(),Email(),Length(max=50)])
+    # email=StringField('Email',validators=[DataRequired(),Email(),Length(max=50)])
     phone=IntegerField('Phone Number',validators=[DataRequired()])
     submit=SubmitField('Submit')
+
+class LiverForm(FlaskForm):
+    username=StringField('Name',validators=[DataRequired(),Length(max=100)])
+    age=IntegerField('Age',validators=[DataRequired()])
+    gender=RadioField('Gender',validators=[DataRequired()],choices=['Male','Female','Others'])
+    address=StringField('Address',validators=[DataRequired(),Length(max=250)])
+    pincode=IntegerField('Pincode',validators=[DataRequired()])
+    total_protein=DecimalField('Total Proteins (g/dL)',validators=[DataRequired()],places=2)
+    albumin=DecimalField('Albumin (g/dL)',validators=[DataRequired()],places=2)
+    ag_ratio=DecimalField('A/G Ratio',validators=[DataRequired()],places=2)
+    total_bilirubin=DecimalField('Total Bilirubin (mg/dl)',validators=[DataRequired()],places=2)
+    direct_bilirubin=DecimalField('Direct Bilirubin (mg/dl)',validators=[DataRequired()],places=2)
+    alkaline_phosphate=IntegerField('Alkaline Phosphate (IU/L)',validators=[DataRequired()])
+    sgpt=IntegerField('SGPT (U/L)',validators=[DataRequired()])
+    sgot=IntegerField('SGOT (U/L)',validators=[DataRequired()])
+    height=DecimalField("Height (in m)",validators=[DataRequired()],places=2)
+    weight=DecimalField("Weight (in kg)",validators=[DataRequired()],places=2)
+    phone=IntegerField('Phone Number',validators=[DataRequired()])
+    submit=SubmitField('Submit')
+
+class KidneyForm(FlaskForm):
+    username=StringField('Name',validators=[DataRequired(),Length(max=100)])
+    gender=RadioField('Gender',validators=[DataRequired()],choices=['Male','Female','Others'])
+    height=DecimalField("Height (in m)",validators=[DataRequired()],places=2)
+    weight=DecimalField("Weight (in kg)",validators=[DataRequired()],places=2)
+    smoke_alco=RadioField('Smoking and Alcohol History',validators=[DataRequired()],choices=['Smoking','Alcohol','Both','None'])
+    age=IntegerField('Age',validators=[DataRequired()])
+    physical_activity=IntegerField('Physical Activity (Weekly)',validators=[DataRequired()])
+    kidney_diet_score=IntegerField('Kidney Diet Score (0-10)',validators=[DataRequired()])
+    genetics=SelectMultipleField('Genetics',validate_choice=[DataRequired()],choices=[('diasease','Family History Diasease'),('hypertension','Family History Hypertension'),('diabetes','Family History Diabetes')])
+    urinary_tract=RadioField('Urinary Tract Infections',validators=[DataRequired()],choices=['Yes','No'])
+    systoyic_bp=IntegerField('Systoyic BP(mmHg)',validators=[DataRequired()])
+    diastotic_bp=IntegerField('Diastotic BP(mmHg)',validators=[DataRequired()])
+    fasting_blood_sugar=DecimalField('Fasting Blood Sugar',validators=[DataRequired()],places=2)
+    hba1clvl=DecimalField("HbA1c Level",validators=[DataRequired()],places=2)
+    serum_creative=DecimalField('Serum Creative(mg/dL)',validators=[DataRequired()],places=2)
+    bunlvl=DecimalField('BUN Level',validators=[DataRequired()],places=2)
+    gfr=DecimalField('GFR (ml/min/1.73m^2)',validators=[DataRequired()],places=2)
+    protein_in_urine=DecimalField('Protein in Urine(g/day)',validators=[DataRequired()],places=2)
+    serum_electrolyes_sodium=DecimalField('Serum Electrolytes Sodium(mEq/L)',validators=[DataRequired()],places=2)
+    serum_electrolyes_potassium=DecimalField('Serum Electrolytes Potassium(mEq/L)',validators=[DataRequired()],places=2)
+    haemoglobin_lvl=DecimalField('Haemoglobin Level(g/dL)',validators=[DataRequired()],places=2)
+    cholestrol_lvl=DecimalField('Cholestrol Level(mg/dL)',validators=[DataRequired()],places=2)
+    diuretics=RadioField('Diuretics',validators=[DataRequired()],choices=['Yes','No'])
+    edema=RadioField('Edema',validators=[DataRequired()],choices=['Yes','No'])
+    muscle=SelectField('Muscle cramps',validators=[DataRequired()],choices=[('low','Low'),('moderate','Moderate'),('high','High')])
+    itching=SelectField('Itching score',validators=[DataRequired()],choices=[('low','Low'),('moderate','Moderate'),('high','High')])
+    submit=SubmitField('Submit')
+
+
+
+class SignUpForm(FlaskForm):
+    username=StringField('Name',validators=[DataRequired(),Length(max=100)])
+    email=StringField('Email',validators=[DataRequired(),Email(),Length(max=50)])
+    password=PasswordField('Password',validators=[DataRequired(),Length(max=6)])
+    submit=SubmitField('Sign Up')
+
+class SignInForm(FlaskForm):
+    email=StringField('Email',validators=[DataRequired(),Email(),Length(max=50)])
+    password=PasswordField('Password',validators=[DataRequired()])
+    submit=SubmitField('Login')
+
+class OtpForm(FlaskForm):
+    otp=IntegerField('OTP',validators=[DataRequired(),Length(max=6)])
+    submit=SubmitField('Verify')
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from app import app
 from flask import render_template
-from app.forms import DiabetesForm
+from app.forms import DiabetesForm,OtpForm,SignInForm,SignUpForm,LiverForm,KidneyForm
 
 @app.route('/home')
 @app.route('/')
@@ -19,24 +19,108 @@ def profile():
 def community():
     return render_template('community.html')
 
+@app.route('/sign-in')
+def signin():
+    signinform=SignInForm()
+    if signinform.validate_on_submit():
+        email=signinform.username.data
+        password=signinform.password.data
+
+    return render_template('signin.html',signin=signinform)
+
+@app.route('/sign-up')
+def signup():
+    signupform=SignUpForm()
+    if signupform.validate_on_submit():
+        username=signupform.username.data
+        email=signupform.email.data
+        password=signupform.password.data
+
+    return render_template('signup.html',signup=signupform)
+
+@app.route('/otp')
+def otp():
+    otpform=OtpForm()
+    if otpform.validate_on_submit():
+        otp=otpform.otp.data
+
+    return render_template('otp.html',otpform=otpform)
+
 @app.route('/diabetes-form')
 def diabetes():
-    myform=DiabetesForm()
-    if myform.validate_on_submit():
-        # Access form data
-        username=myform.username.data
-        gender=myform.gender.data
-        age=myform.age.data
-        address=myform.address.data
+    diabetesform=DiabetesForm()
+    if diabetesform.validate_on_submit():
+        username=diabetesform.username.data
+        gender=diabetesform.gender.data
+        age=diabetesform.age.data
+        address=diabetesform.address.data
 
-        pincode = myform.pincode.data
-        hypertension = myform.hypertension.data
-        previousHeartDisease = myform.previousHeartDisease.data
-        smoking_History = myform.smoking_History.data
-        weight = myform.weight.data
-        height = myform.height.data
-        hba1clvl = myform.hba1clvl.data
-        blood_glucose = myform.blood_glucose.data
-        email = myform.email.data
-        phone = myform.phone.data
-    return render_template('diabetesform.html',form=myform)
+        pincode = diabetesform.pincode.data
+        hypertension = diabetesform.hypertension.data
+        previousHeartDisease = diabetesform.previousHeartDisease.data
+        smoking_History = diabetesform.smoking_History.data
+        weight = diabetesform.weight.data
+        height = diabetesform.height.data
+        hba1clvl = diabetesform.hba1clvl.data
+        blood_glucose = diabetesform.blood_glucose.data
+        email = diabetesform.email.data
+        phone = diabetesform.phone.data
+    return render_template('diabetesform.html',form=diabetesform)
+
+@app.route('/liver-form')
+def liver():
+    liverform=LiverForm()
+    if liverform.validate_on_submit():
+        username = liverform.username.data
+        age = liverform.age.data
+        gender = liverform.gender.data
+        address = liverform.address.data
+        pincode = liverform.pincode.data
+        total_protein = liverform.total_protein.data
+        albumin = liverform.albumin.data
+        ag_ratio = liverform.ag_ratio.data
+        total_bilirubin = liverform.total_bilirubin.data
+        direct_bilirubin = liverform.direct_bilirubin.data
+        alkaline_phosphate = liverform.alkaline_phosphate.data
+        sgpt = liverform.sgpt.data
+        sgot = liverform.sgot.data
+        height = liverform.height.data
+        weight = liverform.weight.data
+        phone = liverform.phone.data
+        
+    
+    return render_template('liver.html',liverform=liverform)
+
+@app.route('/kidney-form')
+def kidney():
+    kidneyform=KidneyForm()
+    if kidneyform.validate_on_submit():
+        username = kidneyform.username.data
+        gender = kidneyform.gender.data
+        height = kidneyform.height.data
+        weight = kidneyform.weight.data
+        smoke_alco = kidneyform.smoke_alco.data
+        age = kidneyform.age.data
+        physical_activity = kidneyform.physical_activity.data
+        kidney_diet_score = kidneyform.kidney_diet_score.data
+        genetics = kidneyform.genetics.data
+        urinary_tract = kidneyform.urinary_tract.data
+        systoyic_bp = kidneyform.systoyic_bp.data
+        diastotic_bp = kidneyform.diastotic_bp.data
+        fasting_blood_sugar = kidneyform.fasting_blood_sugar.data
+        hba1clvl = kidneyform.hba1clvl.data
+        serum_creative = kidneyform.serum_creative.data
+        bunlvl = kidneyform.bunlvl.data
+        gfr = kidneyform.gfr.data
+        protein_in_urine = kidneyform.protein_in_urine.data
+        serum_electrolyes_sodium = kidneyform.serum_electrolyes_sodium.data
+        serum_electrolyes_potassium = kidneyform.serum_electrolyes_potassium.data
+        haemoglobin_lvl = kidneyform.haemoglobin_lvl.data
+        cholestrol_lvl = kidneyform.cholestrol_lvl.data
+        diuretics = kidneyform.diuretics.data
+        edema = kidneyform.edema.data
+        muscle = kidneyform.muscle.data
+        itching = kidneyform.itching.data
+        
+
+    return render_template('kidney.html',kidneyform=kidneyform)
